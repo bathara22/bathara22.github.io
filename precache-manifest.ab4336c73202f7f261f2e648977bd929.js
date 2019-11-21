58c17865cf36c2c336374f81225cbd74bc372e59
= <=>
2019-11-21 14:48:58 +0700

    saya pesan nasi goreng

---
+++ precache-manifest.ab4336c73202f7f261f2e648977bd929.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c28708e08723cdad4c",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "a4c098159eddeb0c623a",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fc2dd074ca4006a901e6ec3e5e5c922f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c28708e08723cdad4c",
+    "url": "/js/about.74de0dce.js"
+  },
+  {
+    "revision": "98502eca85870256dd3c",
+    "url": "/js/app.d8f9a352.js"
+  },
+  {
+    "revision": "a4c098159eddeb0c623a",
+    "url": "/js/chunk-vendors.1f30fe0e.js"
+  },
+  {
+    "revision": "102e15bacd4840b07348369566781699",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
